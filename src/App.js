79ae67eeebbeb86adc626e2c971f18adc10a1542
Gao <xingaoemail@163.com>
2022-05-13 21:13:07 +0800

    feat：redux basic usage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 // import AntdFormPage from "./pages/AntdFormPage";
-import MyRCFieldForm from "./pages/MyRCFieldForm";
+// import MyRCFieldForm from "./pages/MyRCFieldForm";
+import ReduxPage from "./pages/ReduxPage";
 import './App.css';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header> */}
-      <MyRCFieldForm />
+      {/* <MyRCFieldForm /> */}
+      <ReduxPage />
     </div>
   );
 }
