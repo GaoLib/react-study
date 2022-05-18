2a7440d7be773f7caa9982d63bb78cbc29e3a54b
Gao <xingaoemail@163.com>
2022-05-18 22:00:07 +0800

    feat: optimizate combineReducers

--- src/gRedux/combineReducers.js
+++ src/gRedux/combineReducers.js
@@ -7,6 +7,8 @@ export default function combineReducers(reducers) {
       newState[key] = reducer(state[key], action)
       hasChanged = hasChanged || newState !== state[key]
     }
+    // replaceRudecers 对象长度会改变，如 state: {a:1， b:2} -> newState {a: 1}
+    hasChanged = hasChanged || Object.keys(state).length !== Object.keys(newState).length
     return hasChanged ? newState : state
   }
 }
