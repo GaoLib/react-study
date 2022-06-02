1892a0df422325a8123a48ce338fe0d3bf87e83e
Gao <xingaoemail@163.com>
2022-06-02 18:00:37 +0800

    feat: add react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -3,15 +3,38 @@
 // import ReduxPage from "./pages/ReduxPage";
 // import ReactReduxPage from "./pages/ReactReduxPage";
 import ReactReduxHookPage from "./pages/ReactReduxHook";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  // Link,
+  // Switch,
+  // Redirect,
+  // useHistory,
+  // useLocation,
+  // useRouteMatch,
+  // useParams,
+  // withRouter,
+  // Prompt,
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <MyRCFieldForm /> */}
-      {/* <ReduxPage /> */}
-      {/* <ReactReduxPage /> */}
-      <ReactReduxHookPage />
+      <BrowserRouter>
+        <Routes>
+          {/* <MyRCFieldForm /> */}
+          {/* <ReduxPage /> */}
+          {/* <ReactReduxPage /> */}
+          {/* <ReactReduxHookPage /> */}
+          <Route path="/" element={<ReactReduxHookPage />}/>
+        </Routes>
+      </BrowserRouter>
+      {/* <ul>
+        <li><Link to="/reactReduxHook">ReactReduxHookPage</Link></li>
+      </ul> */}
+      
     </div>
   );
 }
