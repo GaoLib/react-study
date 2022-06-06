412950f7039cb5300e7c599835831f7c59c65048
Gao <xingaoemail@163.com>
2022-06-06 16:56:47 +0800

    feat: add pages turn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import AntdFormPage from "./pages/AntdFormPage";
-// import MyRCFieldForm from "./pages/MyRCFieldForm";
-// import ReduxPage from "./pages/ReduxPage";
+import MyRCFieldForm from "./pages/MyRCFieldForm";
+import ReduxPage from "./pages/ReduxPage";
 // import ReactReduxPage from "./pages/ReactReduxPage";
 import ReactReduxHookPage from "./pages/ReactReduxHook";
 import {
   BrowserRouter,
   Routes,
   Route,
-  // Link,
+  Link,
   // Switch,
   // Redirect,
   // useHistory,
@@ -23,18 +23,19 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <div className="links">
+          <Link to="/">MyRCFieldForm</Link>
+          <Link to="/redux">ReduxPage</Link>
+          <Link to="/reactReduxHook">ReactReduxHookPage</Link>
+        </div>
+
         <Routes>
-          {/* <MyRCFieldForm /> */}
-          {/* <ReduxPage /> */}
-          {/* <ReactReduxPage /> */}
+          <Route path="/" element={<MyRCFieldForm />}/>
+          <Route path="/redux" element={<ReduxPage />}/>
           {/* <ReactReduxHookPage /> */}
-          <Route path="/" element={<ReactReduxHookPage />}/>
+          <Route path="/reactReduxHook" element={<ReactReduxHookPage />}/>
         </Routes>
       </BrowserRouter>
-      {/* <ul>
-        <li><Link to="/reactReduxHook">ReactReduxHookPage</Link></li>
-      </ul> */}
-      
     </div>
   );
 }
